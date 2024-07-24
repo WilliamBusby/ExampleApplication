e52d904e5835eda4c28878c191e6399cb44ace27
William Busby <williambusby1999@gmail.com>
2024-07-24 21:43:45 +0100

    Move to src tests structure

---
+++ src/ExampleApplication.Common/Helpers/NotificationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExampleApplication.Common.Helpers
+{
+    public class NotificationHelper : INotificationHelper
+    {
+        public async Task<bool> SendNotification(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface INotificationHelper
+    {
+        public Task<bool> SendNotification(object obj);
+    }
+}
